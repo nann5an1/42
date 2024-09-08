cf3cde97eb11b49981edb5d6f9cf3ac7dc359676
Nann-theDeveloper-001 <nangsuhtetsan94@gmail.com>
2024-09-08 23:04:18 +0800

    exam_commit

--- exam/examRank02/Level 2/inter.c
+++ exam/examRank02/Level 2/inter.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/21 18:26:56 by marvin            #+#    #+#             */
-/*   Updated: 2024/08/21 18:26:56 by marvin           ###   ########.fr       */
+/*   Created: 2024/09/08 22:15:09 by marvin            #+#    #+#             */
+/*   Updated: 2024/09/08 22:15:09 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,24 @@
 int main(int argc, char** argv)
 {
     int i = 0;
-    int j = 0;
-    int len = 0;
+    int j;
     if(argc == 3)
     {
-        while(argv[1][len] != '\0')
-            len++;
-        while(--len)
-        {
-            if(argv[1][i] == argv[1][len])
-                
-        }
+        unsigned char seen[256] = {0};
         while(argv[1][i] != '\0')
         {
             j = 0;
-            while(argv[2][j] != '\0')
+            while(argv[2][j])
             {
                 if(argv[1][i] == argv[2][j])
-                    write(1, &argv[1][i], 1);
+                {
+                    if(!seen[(unsigned char)argv[1][i]])
+                    {
+                        write(1, &argv[1][i], 1);
+                        seen[(unsigned char)argv[1][i]] = 1;
+                    }
+                    break;
+                }
                 j++;
             }
             i++;
