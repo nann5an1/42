619b7a36519f9019013ae6c158bbfcbc485acc2c
Nann-theDeveloper-001 <nangsuhtetsan94@gmail.com>
2024-09-14 23:36:40 +0800

    pipex

--- pipex/pipex.c
+++ pipex/pipex.c
@@ -12,38 +12,85 @@
 
 #include <stdio.h>
 #include <unistd.h>
-int main()
+#include <stdlib.h>
+#include <fcntl.h>
+
+void exec_command()
+{
+
+}
+
+
+//user compilation --> ./pipex.exe "in_file" "ls -l" "grep hello" "out_file"
+int main(int argc, char** argv)
 {
     int fd[2];
     pid_t pid1;
+    int in_file;
+    int out_file;
+    if(argc == 5)
+    {
+        //creation of pipe
+        if(pipe(fd) < 0){
+            perror("Error in creating pipe");
+            exit(EXIT_FAILURE);
+        }
 
-    //create a pipe
-    if(pipe(fd) < 0)
-        perror("Error in creating pipe");
+        in_file = open(argv[1], O_RDONLY);
+        out_file = open(argv[4], O_WRONLY | O_CREAT, 0644);
+
+        if(in_file < 0 || out_file < 0)
+        {
+            perror("Error in file handling");
+            exit(EXIT_FAILURE);
+        }
+        exec_command(argv[2]);
+        exec_command(argv[3]);
+
+        //create child and parent process using fork for the first command
+        int res;
+        pid1 = fork();
+        if(pid1 < 0)
+        {
+            perror("Error in creating child");
+            exit(EXIT_FAILURE);
+        }
+             
+        else if(pid1 == 0)
+        {
+            printf("Child process created");
+            //additional function which does the file searching under directories and return full path
+            char* filename = argv[1];
+            const char* filePath = dirSearch(filename);
+            res = access(filePath, F_OK);
+            if(res < 0)
+            {
+                perror("File accessing error");
+                exit(EXIT_FAILURE);
+            }
+            //file accessing is okay (permissions are allowed)
+            char *const av[] = ft_split(argv[2]);
+            const char* programDir = programSearch(av[0]);
+            const char* envp[] = {NULL}; //path to the program i want to execute
+                
+            //redirectiong ouput to the pipe
+            dup2(fd[1], STDOUT_FILENO);
+            close(fd[0]);
+            close(fd[1]);
+
+            if(execve(programDir, av, envp) < 0)
+                perror("Error Executing");
+        }
+        else
+        {
+            printf("Parent process created");
+            close(fd[1]);
+            waitpid(pid1, NULL, 0);
+        }         
+    }
+}
     
-    else if(pipe(fd) == 0)
-    {
-        printf("Pipe success");
-    }
-
-    //create child and parent process using fork for the first command
-    pid1 = fork();
-    if(pid1 < 0)
-        perror("Error in creating child");
-    else if(pid1 == 0)
-    {
-        //using of access() function
-        printf("Child process created");
-        //usage of dup()
-        //close the un-necessary ends of pipes (even for the duplicated ones)
-        // execve(); //execute the command for the child process
-    }
-    else
-        printf("Parent process created");
-
 
     //create child and parent process using fork for the second command
-    //at the very end add waitpid(pid1) to wait for pid1 to finish
     
-
-}
+    //at the very end add waitpid(pid1) to wait for pid1 to finish
