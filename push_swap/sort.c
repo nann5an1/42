5c82f79dc3faed494e3a4cf3db6b93595b1de0ca
Nann-theDeveloper-001 <nangsuhtetsan94@gmail.com>
2024-10-06 22:07:17 +0800

    push_swap

--- push_swap/sort.c
+++ push_swap/sort.c
@@ -12,6 +12,13 @@
 
 #include "push_swap.h"
 
+void main_sort4(t_struct_node **stackA, t_struct_node *stackB)
+{
+    push(&stackB, stackA);
+    sort_three(stackA);
+    push(stackA, &stackB);
+}
+
 void sort_three(t_struct_node **stack)
 {
     int min;
@@ -44,37 +51,27 @@ void sort_three(t_struct_node **stack)
 void    sort_four(t_struct_node **stackA)
 {
     t_struct_node   *stackB;
-    int min_index;
-    int max_index;
     
     stackB = NULL;
-    
-    if (is_sorted(*stackA) == 1)
-    {
-        printf("The stack 4 is already sorted");
-        return ;
-    }
-
     while (is_sorted(*stackA) != 1)
     {
-        min_index = minNode(*stackA);
-        max_index = maxNode(*stackA);
-
-        if (min_index == 0 || max_index == 0)
-            push(&stackB, stackA);
-        if (min_index == 3 || max_index == 3)
+        if (maxNode(*stackA) == 0 || minNode(*stackA) == 0)
+        {
+            main_sort4(stackA, stackB);
+            if (is_sorted(*stackA) == 1)
+                break;
+            rotate(stackA);
+        }
+        else if (minNode(*stackA) == 3 || maxNode(*stackA) == 3)
+        {
             r_rotate(stackA);
+            if (is_sorted(*stackA) == 1)
+                break;
+            main_sort4(stackA, stackB);
+            rotate(stackA);
+        }
         else
             swap(stackA);
-        if (getLength(*stackA) <= 3)
-        {
-            sort_three(stackA);
-            while (stackB)
-            {
-                push(stackA, &stackB);
-            }
-        }
     }
-    printf("Sorted stack in sort 4:\n");
     show_lst(*stackA);
 }
