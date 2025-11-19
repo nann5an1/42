a503ecf1daadc0fd604ce4a72719e82fdd972863
Nann-theDeveloper-001 <nangsuhtetsan94@gmail.com>
2025-11-19 18:08:43 +0800

    server/location scopes validation and insertion

--- webserv/webserv-nsan/include/Server.hpp
+++ webserv/webserv-nsan/include/Server.hpp
@@ -4,22 +4,24 @@
 #include <iostream>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <sys/epoll.h>
+// #include <sys/epoll.h>
 #include <fcntl.h>
 #include <errno.h>  // For errno
 #include <cstddef>
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/epoll.h>
 
 class Server{
     private:
-        int socket_fd; //endpoint for communication
+        // int socket_fd; //endpoint for communication
     public:
         Server();
         Server(const Server &other);
         ~Server();
         Server &operator=(const Server &other);
         void initiate();
+};
 
 #endif
